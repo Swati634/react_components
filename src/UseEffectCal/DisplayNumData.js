744671ef4200b166da0e784931704ab87ccd1da3
Swati634 <120095594+Swati634@users.noreply.github.com>
2023-01-18 18:21:01 +0530

    Two numbers of addtion using usestack

---
+++ src/UseEffectCal/DisplayNumData.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import InputNum from './InputNum'
+
+const DisplayNumData = () => {
+    return (
+        <div>
+            <center>
+                <InputNum />
+            </center>
+        </div >
+    )
+}
+
+export default DisplayNumData
---
+++ src/UseEffectCal/Mul.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mul = (props) => {
+    return (
+        <div>
+            <h3>Multiplication: {props.numb1 * props.numb2}</h3>
+        </div>
+    )
+}
+
+export default Mul
